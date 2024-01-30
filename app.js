b6e38b3a44db1b1644c021694b5504801c10a35a
Gabriel Maia <gribeiromaia736@gmail.com>
2024-01-30 00:19:00 -0300

    add Post method for middleware test

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
-const app = express()
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false }));
 
 app.use('/add-product', (req, res, next) => {
   console.log('Continue to next middleware')
-  res.send('Testando o outro middleware')
+  res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit"> Add</button></form>')
+})
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/')
 })
 
 app.use('/', (req, res, next) => {
