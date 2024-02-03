d5418cde004b27413fdf6c65d024a190747a6498
Gabriel Maia <gribeiromaia736@gmail.com>
2024-02-02 23:09:47 -0300

    creating page not found

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path')
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false }));
 
-app.use('/admin',adminRoutes); // /admin/add-product
+app.use('/admin', adminRoutes); // /admin/add-product
 app.use(shopRoutes);
 
 //Pode se fazer um try catch
 app.use((req, res, next) => { //Aqui temos o tratamento para qd não retornar nada na url
-  res.status(404).send('<h1>Page not Found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000);
