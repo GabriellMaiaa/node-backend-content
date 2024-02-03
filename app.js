aa0a5a75e552e6d6031c72e791ae3039d5b67ff3
Gabriel Maia <gribeiromaia736@gmail.com>
2024-02-02 22:14:51 -0300

    page not found created and new file path

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
-app.use(shopRoutes)
-app.use(adminRoutes)
-
 app.use(bodyParser.urlencoded({extended: false }));
+
+app.use('/admin',adminRoutes); // /admin/add-product
+app.use(shopRoutes);
+
+//Pode se fazer um try catch
+app.use((req, res, next) => { //Aqui temos o tratamento para qd não retornar nada na url
+  res.status(404).send('<h1>Page not Found</h1>')
+})
 
 app.listen(3000);
